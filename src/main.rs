5902803850c57d88b4401cdda7c8eef87516d1be
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-04-06 22:26:00 -0300

    Add file verification and parse into lines

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, str::FromStr};
+use std::{env, fmt::Display, str::FromStr};
 
 use argparse_rs::{ArgParser, ArgType};
 
@@ -9,7 +9,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let parsed_arguments = parse_arguments(&args, &mut parser);
-    println!("{:?}", parsed_arguments)
+    let absolute_filepath = get_absolute_filepath(&parsed_arguments.filepath);
+
+    verify_filepath(&absolute_filepath);
+
+    let file_content = open_file(&absolute_filepath);
+
+    for line in file_content {
+        println!("+ {}", line);
+    }
 }
 
 fn configure_parser(parser: &mut ArgParser) {
@@ -38,6 +46,34 @@ fn parse_arguments(args: &Vec<String>, parser: &mut ArgParser) -> Args {
     }
 }
 
+fn get_absolute_filepath(filename: &str) -> String {
+    format!(
+        "{}/{}",
+        env::current_dir().unwrap().as_path().to_str().unwrap(),
+        filename
+    )
+}
+
+fn verify_filepath(filepath: &str) {
+    let metadata = std::fs::metadata(filepath);
+
+    if metadata.is_err() {
+        panic!("Cannot access file {}", filepath);
+    }
+
+    if !metadata.unwrap().is_file() {
+        panic!("Must specify a valid file");
+    };
+}
+
+fn open_file(filepath: &str) -> Vec<String> {
+    std::fs::read_to_string(&filepath)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
+}
+
 #[derive(Debug)]
 struct Args {
     filter: ImageFilter,
@@ -55,6 +91,14 @@ impl FromStr for ImageFilter {
         return match s {
             "grayscale" => Ok(ImageFilter::GrayScale),
             _ => Err(()),
+        };
+    }
+}
+
+impl Display for ImageFilter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return match self {
+            ImageFilter::GrayScale => write!(f, "grayscale"),
         };
     }
 }
